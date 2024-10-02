55148752ea61f46ca0daa1ee03a002f46a662af2
Fanny BOUSSARD <boussard.fanny@gmail.com>
2024-10-02 18:28:52 +0200

    ajout nettoyage cache et amelioration gestion des couches dans dockerfile + dockerignor + exec le script pour wordpress

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -10,20 +10,16 @@ fi
 # Start the MySQL daemon in the background
 mysqld_safe --skip-networking &
 
-# Wait for MySQL to be fully up and running
 until mysqladmin ping &>/dev/null; do
   echo -n "."; sleep 1
 done
 
-# Execute the SQL commands
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
-# Shutdown MySQL
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 
-# Start MySQL in the foreground
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-# Si le fichier wp-config.php n'existe pas dans le répertoire /var/www/html, changer le répertoire courant à /var/www/html
 if [ ! -f /var/www/html/wp-config.php ]; then
     cd /var/www/html
 fi
 
-# Vérifier si WordPress est installé
 if ! wp core is-installed --path="$WP_PATH" --allow-root; then
-  # Télécharger WordPress si non installé
     wp core download --locale=en_GB --path="$WP_PATH" --allow-root
 
-  # Créer le fichier de configuration de WordPress
     wp config create --allow-root --dbname="$SQL_DATABASE" --dbuser="$SQL_USER" --dbpass="$SQL_PASSWORD" \
                      --dbhost=mariadb --path="$WP_PATH" --skip-check
 
-  # Installer WordPress
     wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
                     --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="$WP_PATH" --allow-root
 
-  # Créer un nouvel utilisateur WordPress
     wp user create --allow-root "$WP_USER" "$WP_EMAIL" --user_pass="$WP_PASS" --path="$WP_PATH" --allow-root
 fi
 
-# Exécuter les commandes supplémentaires passées au script
-exit 0
+exec "$@"
