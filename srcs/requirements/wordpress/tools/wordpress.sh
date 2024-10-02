77bf322c10a719041aa689d304bea7c653943eb1
Boussard Fanny <boussard.fanny@gmail.com>
2024-10-02 15:56:57 +0200

    changements ports

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Attendre que MariaDB démarre
 sleep 10
 
-# Check if WordPress is already installed
-if ! $(wp core is-installed --path=$WP_PATH --allow-root); then
+# Vérifier si WordPress est déjà installé
+if ! wp core is-installed --path=$WP_PATH --allow-root; then
+  # Télécharger WordPress si non installé
   wp core download --locale=en_GB --path=$WP_PATH --allow-root
 
-  # Ensure the WordPress installation path is writable
+  # S'assurer que le répertoire d'installation est accessible en écriture
   chmod -R 755 $WP_PATH
 
+  # Créer le fichier de configuration wp-config.php
   wp config create \
     --dbname=$SQL_DATABASE \
     --dbuser=$SQL_USER \
@@ -18,15 +21,17 @@ if ! $(wp core is-installed --path=$WP_PATH --allow-root); then
     --allow-root \
     --skip-check
 
+  # Installer WordPress
   wp core install \
     --url=$WP_URL \
-    --title=$WP_TITLE \
+    --title="$WP_TITLE" \
     --admin_user=$WP_ADMIN_USER \
     --admin_email=$WP_ADMIN_EMAIL \
     --admin_password=$WP_ADMIN_PASS \
     --path=$WP_PATH \
     --allow-root
 
+  # Créer un utilisateur supplémentaire
   wp user create \
     $WP_USER \
     $WP_EMAIL \
@@ -35,4 +40,6 @@ if ! $(wp core is-installed --path=$WP_PATH --allow-root); then
     --allow-root
 fi
 
-exec "$@"
+# Run PHP
+mkdir -p /run/php
+php-fpm7.3 -F
