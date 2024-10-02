185a3bd960568f67a1a79debd85137db618e5d99
Boussard Fanny <boussard.fanny@gmail.com>
2024-10-02 13:11:06 +0200

    Fixed (mariadb): -p${SQL_ROOT_PASSWORD} is added to each mysql command

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -16,11 +16,11 @@ until mysqladmin ping &>/dev/null; do
 done
 
 # Execute the SQL commands
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 # Shutdown MySQL
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
