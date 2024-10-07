06171e8544ab3bea977933568bd3fe78de33de3a
fanny <boussard.fanny@gmail.com>
2024-10-07 13:11:11 +0200

    wordpress : fixed  - silence errors

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -4,16 +4,16 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     cd /var/www/html
 fi
 
-if ! wp core is-installed --path="$WP_PATH" --allow-root; then
-    wp core download --locale=en_GB --path="$WP_PATH" --allow-root
+if ! wp core is-installed --path="$WP_PATH" --allow-root 2>/dev/null; then
+    wp core download --locale=en_GB --path="$WP_PATH" --allow-root 2>/dev/null
 
     wp config create --allow-root --dbname="$SQL_DATABASE" --dbuser="$SQL_USER" --dbpass="$SQL_PASSWORD" \
-                     --dbhost=mariadb --path="$WP_PATH" --skip-check
+                     --dbhost=mariadb --path="$WP_PATH" --skip-check 2>/dev/null
 
     wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
-                    --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="$WP_PATH" --allow-root
+                    --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="$WP_PATH" --allow-root 2>/dev/null
 
-    wp user create --allow-root "$WP_USER" "$WP_EMAIL" --user_pass="$WP_PASS" --path="$WP_PATH" --allow-root
+    wp user create --allow-root "$WP_USER" "$WP_EMAIL" --user_pass="$WP_PASS" --path="$WP_PATH" --allow-root 2>/dev/null
 fi
 
-exec "$@"
+exec "$@" 2>/dev/null
